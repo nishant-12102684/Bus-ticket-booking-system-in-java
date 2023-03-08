ed7864cc1d62bf0fc73c55bfc0f579fd36438526
nishant-12102684 <125746135+nishant-12102684@users.noreply.github.com>
2023-03-08 21:19:59 +0530

    Update and rename BusTicketSystem to BusTicketSystem.java

---
+++ BusTicketSystem.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class BusTicketSystem{
+         public static void main(String args[]) {
+                  Scanner sc= new Scanner(System.in);
+                  String user,password;
+                  for(int i=1;i<4;){
+		System.out.print("Enter Username: ");
+		String user= sc.nextLine();
+		System.out.print("Enter Password: ");
+		String password= sc.nextLine();
+
+
