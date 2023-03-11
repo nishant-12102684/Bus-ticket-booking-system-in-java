d818334589efa01c5e6b0d36b916cdd976d9091d
nishant-12102684 <125746135+nishant-12102684@users.noreply.github.com>
2023-03-11 19:30:18 +0530

    Update BusTicketSystem.java

--- BusTicketSystem.java
+++ BusTicketSystem.java
@@ -3,11 +3,26 @@ import java.util.*;
 public class BusTicketSystem{
          public static void main(String args[]) {
                   Scanner sc= new Scanner(System.in);
-                  String user,password;
+                  String user,password,choice;
                   for(int i=1;i<4;){
 		System.out.print("Enter Username: ");
 		String user= sc.nextLine();
 		System.out.print("Enter Password: ");
 		String password= sc.nextLine();
+		if(user.equals("1") && password.equals("1")){
+			for(int x=1; x==1;){
+    			System.out.println(" [1] Destination                   ");
+    			System.out.println(" [2] Passengers                    ");
+    			System.out.println(" [3] Billing                       ");
+    			System.out.println(" [4] View                          ");
+    			System.out.println(" [5] Exit                          ");
+				
+			for(x=1; x==1;){
+					System.out.print("ENTER CHOICE: ");
+					choice=in.readLine();
+			}
+				
+			}			
+    			
 
 
